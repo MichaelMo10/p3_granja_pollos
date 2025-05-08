adfcaa0a8d6b5ce0d78df527cbcc16c242cf11bb
MichaelMo10 <michaelmora945@gmail.com>
2025-05-08 11:16:44 -0500

    Update Base_datos.py

--- Base_datos.py
+++ Base_datos.py
@@ -1,11 +1,11 @@
 Class Base_datos:
  def _init_(self):
-   self.lista_datos = [("P001", "pequeña", "15dias"),("P002", "grande", "5dias")]
+   self.lista_datos = [("P001", "pequeña", "15dias"),("P002", "mediano", "10dias"), ("P003", "grande", "5dias")]
 
 def imprimir_info(self):
   for self.lista_datos in range(self.lista_datos.len):
-    for(info_pollo in self.lista_datos)
-      print(info_pollo)
-
+   for(info_pollo in self.lista_datos)
+     print(info_pollo)
+    
 
 def 
