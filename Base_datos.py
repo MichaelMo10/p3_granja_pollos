bf6cfb6b684f5e20c0f6d898285887ecaf989d97
MichaelMo10 <michaelmora945@gmail.com>
2025-05-08 11:07:57 -0500

    Create Base_datos.py

---
+++ Base_datos.py
@@ -0,0 +1,11 @@
+Class Base_datos:
+ def _init_(self):
+   self.lista_datos = [("P001", "pequeña", "15dias"),("P002", "grande", "5dias")]
+
+def imprimir_info(self):
+  for self.lista_datos in range(self.lista_datos.len):
+    for(info_pollo in self.lista_datos)
+      print(info_pollo)
+
+
+def 
